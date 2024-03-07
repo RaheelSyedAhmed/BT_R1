f76a8c21f52f8e0b85969a461dc9c73518d1c430
RaheelSyedAhmed <raheelsyedahmed@gmail.com>
2024-03-07 13:30:13 -0600

    Adjusted Plot commands
    Changed DI pairs to 1.5 L  and changed textread to have paths specific to Cry1Ab remake.

--- plot_commands.m
+++ plot_commands.m
@@ -2,14 +2,14 @@ monomer_6dj4 = textread("Cry1Ab/Structural_Info/monomer_6dj4_allatom_8");
 intradomain_N_DI = textread("Cry1Ab/Domains/Endotoxin_N/Endotoxin_N_align_ranked_matched.DI");
 intradomain_M_DI = textread("Cry1Ab/Domains/Endotoxin_M/Endotoxin_M_align_ranked_matched.DI");
 intradomain_C_DI = textread("Cry1Ab/Domains/Endotoxin_C/Endotoxin_C_align_ranked_matched.DI");
-%interdomain_NM_DI = textread("PFam_Domains/BTR1_concat_3_NM_ranked_mapped.DI");
-%interdomain_NC_DI = textread("PFam_Domains/BTR1_concat_3_NC_ranked_mapped.DI");
-%interdomain_MC_DI = textread("PFam_Domains/BTR1_concat_3_MC_ranked_mapped.DI");
+interdomain_NM_DI = textread("Cry1Ab/Domains/domains_concat/Endotoxin_concat_NM_ranked_mapped.DI");
+interdomain_NC_DI = textread("Cry1Ab/Domains/domains_concat/Endotoxin_concat_NC_ranked_mapped.DI");
+interdomain_MC_DI = textread("Cry1Ab/Domains/domains_concat/Endotoxin_concat_MC_ranked_mapped.DI");
 %%
 plotDCAmap(monomer_6dj4, [], [1,600], 0, 1);
-plotDCAmap(intradomain_N_DI(1:350, :), [], [1,600], 0, 0);
-plotDCAmap(intradomain_M_DI(1:350, :), [], [1,600], 0, 0);
-plotDCAmap(intradomain_C_DI(1:350, :), [], [1,600], 0, 0);
+plotDCAmap(intradomain_N_DI(1:300, :), [], [1,600], 0, 0);
+plotDCAmap(intradomain_M_DI(1:300, :), [], [1,600], 0, 0);
+plotDCAmap(intradomain_C_DI(1:200, :), [], [1,600], 0, 0);
 %%
 plotDCAmap(interdomain_NM_DI(1:100, :), [], [1,600], 0, 0);
 plotDCAmap(interdomain_MC_DI(1:100, :), [], [1,600], 0, 0);
